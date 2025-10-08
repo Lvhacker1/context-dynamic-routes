40ad0db7dad3452c5dcb5131bd62b8aa0d4dbee3
Lvhacker1 <lavasha2@hotmail.com>
2025-10-08 22:49:35 +0200

    add data

---
+++ data/data.ts
@@ -0,0 +1,7 @@
+import {  NavLinks } from "@/types/types";
+
+export const navlinkData: NavLinks[] = [
+    {href: '/', label: 'Home'},
+    {href: '/categories', label: 'CA'},
+    {href: '/profile', label: 'PR'}
+]
