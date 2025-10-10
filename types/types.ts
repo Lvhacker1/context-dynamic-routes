d8bd113a78745a93957d46bf2cec4228787adfed
Lvhacker1 <lavasha2@hotmail.com>
2025-10-10 14:33:29 +0200

    add types

--- types/types.ts
+++ types/types.ts
@@ -8,3 +8,12 @@ export interface NavbarConfig {
     welcomeText: string;
     logoutButton: string;
 }
+
+export interface Users {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    savedMeals: string[];
+    favoriteCategory: string | null;
+}
