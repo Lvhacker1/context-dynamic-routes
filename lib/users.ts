6949d30a62926ed588ad7f4764e038da1939bbb8
Lvhacker1 <lavasha2@hotmail.com>
2025-10-10 14:51:50 +0200

    add users data

---
+++ lib/users.ts
@@ -0,0 +1,28 @@
+import { Users } from "@/types/types";
+
+export const users: Users[] = [
+    {
+        id: '1',
+        name: 'Lava',
+        email: 'lavasha2@hotmail.com',
+        password: 'blabla',
+        savedMeals: [],
+        favoriteCategory: null,
+    },
+    {
+        id: '2',
+        name: 'Mariam',
+        email: 'mariam@hotmail.com',
+        password: 'blabla2',
+        savedMeals: [],
+        favoriteCategory: null,
+    },
+    {
+        id: '3',
+        name: 'Rob',
+        email: 'rob@hotmail.com',
+        password: 'blabla3',
+        savedMeals: [],
+        favoriteCategory: null,
+    },
+]
