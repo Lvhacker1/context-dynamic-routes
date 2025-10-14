451f74d93a47e4e435de12803170295b8b9fec41
Lvhacker1 <lavasha2@hotmail.com>
2025-10-14 15:41:08 +0200

    create category-page

---
+++ app/categories/[categoryName]/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar"
+
+
+const CategoryPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <main>
+
+            </main>
+        </div>
+    )
+}
+
+export default CategoryPage
