744110bc9d592651c4f4df7d08d3a87b6ffb36ba
Lvhacker1 <lavasha2@hotmail.com>
2025-10-14 16:35:28 +0200

    add category-page with meal and images

--- app/categories/[categoryName]/page.tsx
+++ app/categories/[categoryName]/page.tsx
@@ -1,12 +1,50 @@
+'use client'
 import Navbar from "@/components/Navbar"
+import { useUser } from "@/context/UserContext";
+import { getMealsByCategory } from "@/lib/api";
+import { MealPreview } from "@/types/types";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react";
 
 
 const CategoryPage = () => {
+    const params = useParams();
+    const categoryName = params.categoryName as string;
+    const {user} = useUser();
+    const [meals, setMeals] = useState<MealPreview[]> ([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const cat = categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+        getMealsByCategory(cat).then(data => {
+            console.log("Fetched meals:", data);
+            setMeals(data); setLoading(false);})
+    }, [categoryName])
+
+    if (!user) return null;
+    if (loading) return <div><Navbar /> <p>Loading...</p></div>
     return (
         <div>
             <Navbar />
             <main>
-
+                <div>
+                    <Link href='/categories'>Back to categories</Link>
+                    <h1>{categoryName}</h1>
+                    <div>
+                        {meals.map(meal => (
+                            <Link key={meal.idMeal} href={`/meal/${meal.idMeal}`}>
+                                <div>
+                                    <Image src={meal.strMealThumb} alt={meal.strMeal} width={200} height={100} />
+                                </div>
+                                <div>
+                                    <h3>{meal.strMeal}</h3>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
             </main>
         </div>
     )
