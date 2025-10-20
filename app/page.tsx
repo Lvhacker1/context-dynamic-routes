5c6231f40494461d9f744d058d55d32fa642860c
Lvhacker1 <lavasha2@hotmail.com>
2025-10-20 17:18:31 +0200

    add bg image to content div and style links

--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,18 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <main className="flex flex-col justify-center min-h-screen w-full bg-center sm:bg-top bg-no-repeat bg-cover "
+      <main className=" text-black flex flex-col justify-center min-h-screen w-full bg-center sm:bg-top bg-no-repeat bg-cover "
             style={{backgroundImage: 'url(/rec6.png)'}}
       >
-        <div>
+        <div className="flex flex-col gap-2 mx-auto px-6 py-10" style={{backgroundImage: 'url(/note4.png)'}}>
           <h1 className="text-2xl text-center">{homePageContent.title} {user.name}</h1>
           <p>{homePageContent.subTitle}</p>
-          <div>
+          <div className="flex gap-2 mt-4 justify-center">
             {homePageContent.links.map((link) => (
-              <Link 
+              <Link className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-1 px-4 rounded transition-colors duration-200"
               key={link.href}
               href={link.href}>
-                <p>{link.description} <span className="ml-2">➜</span></p>
+                <p>{link.description} <span className="ml-1">➜</span></p>
               </Link>
             ))}
           </div>
