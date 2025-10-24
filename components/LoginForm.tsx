1e58b7fc14e15734f1504b1df9225e28214d7a56
Lvhacker1 <lavasha2@hotmail.com>
2025-10-24 16:12:22 +0200

    add bg image

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -2,6 +2,7 @@
 import { useUser } from "@/context/UserContext"
 import { loginFormData } from "@/data/data";
 import { users } from "@/lib/users";
+import Image from "next/image";
 import { FormEvent, useState } from "react";
 
 
@@ -20,12 +21,20 @@ const LoginForm = () => {
 
 
     return (
-        <div className="flex flex-col h-screen justify-center items-center">
-            <div className="flex flex-col gap-4 justify-center items-center w-full">
-                <h1 className="text-3xl text-shadow-white">{loginFormData.pageTitle}</h1>
-                <div className="p-10 bg-gray-100 w-full max-w-2xl text-gray-600">
-                    <p className="text-2xl">{loginFormData.subTitle}</p>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-7 bg-gray-100 pt-10 rounded-xs w-full max-w-3xl">
+        <div className="flex flex-col justify-center items-center" 
+        style={{
+            backgroundImage: 'url(/food55.png)', 
+            minHeight: '100vh', 
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center', 
+            backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+            backgroundBlendMode: 'overlay'
+            }}>
+            <div className="flex flex-col gap-4 justify-center items-center w-full h-screen p-4">
+                <h1 className="text-2xl sm:text-3xl text-shadow-white">{loginFormData.pageTitle}</h1>
+                <div className="p-4 sm:p-10 bg-gray-100 w-full max-w-xl rounded-sm">
+                    <p className="text-xl sm:text-2xl  text-gray-700">{loginFormData.subTitle}</p>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-7 bg-gray-100 pt-10 rounded-xs w-full max-w-3xl text-gray-600">
                     <div className="border-b-2 flex flex-col">
                         <label htmlFor="email" >{loginFormData.email}</label>
                         <input className="focus:outline-none" type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} required />
@@ -35,7 +44,7 @@ const LoginForm = () => {
                         <input className="focus:outline-none" type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} required />
                     </div>
                     {error && <p>{error}</p>}
-                    <button className="py-2 px-4 bg-red-700 rounded-md text-white w-fit mt-4" type="submit">{loginFormData.submitButton}</button>
+                    <button className="py-2 px-4 bg-red-700 rounded-md text-white w-fit mt-4 font-bold" type="submit">{loginFormData.submitButton}</button>
                 </form>
                 </div>
                 <div className="flex gap-2">
