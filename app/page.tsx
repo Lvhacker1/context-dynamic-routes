e4e16c4c7d7cda6c3c8b1a96053c3977f90e537c
Lvhacker1 <lavasha2@hotmail.com>
2025-10-24 17:25:57 +0200

    restyle homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useUser } from "@/context/UserContext";
 import { homePageContent } from "@/data/data";
 import Image from "next/image";
 import Link from "next/link";
+import { PiHandWaving } from "react-icons/pi";
 
 export default function Home() {
   const {user} = useUser();
@@ -13,27 +14,27 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <main className=" text-white flex flex-col justify-center">
-        <div className="flex flex-col gap-2 mx-auto px-6 py-10">
-          <h1 className="text-2xl text-center">{homePageContent.title} {user.name}</h1>
-          <p className="text-center">{homePageContent.subTitle}</p>
-          <p>{homePageContent.linkText}</p>
-          <div className="flex gap-2 mt-4 justify-center">
+      <main className=" text-white flex flex-col-reverse justify-end items-center min-h-screen md:flex-row md:items-center">
+        <div className="flex flex-col justify-center items-start text-left p-4 w-full md:w-1/2 lg:p-16">
+          <h1 className="flex items-center gap-4 text-3xl font-bold lg:text-5xl">{homePageContent.title} {user.name} <PiHandWaving size={32} /></h1>
+          <p className="text-lg mb-6 md:text-xl">{homePageContent.subTitle}</p>
+          <p className="text-md mb-4 sm:text-lg font-semibold text-gray-300">{homePageContent.linkText}</p>
+          <div className="flex flex-wrap gap-4">
             {homePageContent.links.map((link) => (
-              <Link className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-1 px-4 rounded transition-colors duration-200"
+              <Link className="bg-red-600 hover:bg-red-700 text-white font-semibold py-1 px-4 rounded transition-colors duration-300"
               key={link.href}
               href={link.href}>
                 <p>{link.description} <span className="ml-1">➜</span></p>
               </Link>
             ))}
           </div>
-          <div className="w-full h-100 relative md:w-1/2 md:h.screen"> 
-            <Image 
-              src="/food55.png"
-              alt="food image"
-              fill
-              />
-          </div>
+        </div>
+        <div className="w-full h-80 relative md:w-1/2 md:h-screen"> 
+          <Image 
+            src="/food55.png"
+            alt="food image"
+            fill
+            />
         </div>
       </main>
     </div>
