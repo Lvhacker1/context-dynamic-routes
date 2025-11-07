47e1386d42b54e7f660cab989574dc5d2360d49c
Lvhacker1 <lavasha2@hotmail.com>
2025-11-07 15:01:56 +0100

    update error message in useUser hook

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -83,6 +83,6 @@ export function UserProvider({children}: {children: ReactNode}) {
 export function useUser() {
     const context = useContext(UserContext)
     if (!context)
-        throw new Error ('blablabal');
+        throw new Error ('useUser must be used within a UserProvider');
     return context;
 }
